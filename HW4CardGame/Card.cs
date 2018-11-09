04b4081ed59a13ee20c440cdfb903b4dbfbdf1bb
Ricky Yang <yangrhy@gmail.com>
2018-11-09 00:53:24 -0600

    Added classes for card game

---
+++ HW4CardGame/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4CardGame
+{
+    class Card
+    {
+        private string face { get; }
+        private string suit { get; }
+
+        public Card(string cardFace, string cardSuit)
+        {
+            face = cardFace;
+            suit = cardSuit;
+        }
+
+        public override string ToString() => $"{face} of {suit}";
+        
+    }
+}

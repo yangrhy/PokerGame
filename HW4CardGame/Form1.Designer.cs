f4b757d9b117ece65e386947ceaf038fb69519d3
Ricky Yang <yangrhy@gmail.com>
2018-11-19 14:55:29 -0600

    Added visuals and objects to form1

--- HW4CardGame/Form1.Designer.cs
+++ HW4CardGame/Form1.Designer.cs
@@ -28,13 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.hand1PicBox5 = new System.Windows.Forms.PictureBox();
+            this.hand1PicBox4 = new System.Windows.Forms.PictureBox();
+            this.hand1PicBox3 = new System.Windows.Forms.PictureBox();
+            this.hand1PicBox2 = new System.Windows.Forms.PictureBox();
+            this.hand1PicBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.hand2PicBox5 = new System.Windows.Forms.PictureBox();
+            this.hand2PicBox4 = new System.Windows.Forms.PictureBox();
+            this.hand2PicBox3 = new System.Windows.Forms.PictureBox();
+            this.hand2PicBox2 = new System.Windows.Forms.PictureBox();
+            this.hand2PicBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateHandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateHand1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateHand2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleName = "";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 411);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = global::HW4CardGame.Properties.Resources.poker_table;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.hand1PicBox5);
+            this.tabPage1.Controls.Add(this.hand1PicBox4);
+            this.tabPage1.Controls.Add(this.hand1PicBox3);
+            this.tabPage1.Controls.Add(this.hand1PicBox2);
+            this.tabPage1.Controls.Add(this.hand1PicBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 385);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Player 1 Hand";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // hand1PicBox5
+            // 
+            this.hand1PicBox5.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand1PicBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand1PicBox5.Location = new System.Drawing.Point(520, 129);
+            this.hand1PicBox5.Name = "hand1PicBox5";
+            this.hand1PicBox5.Size = new System.Drawing.Size(64, 80);
+            this.hand1PicBox5.TabIndex = 4;
+            this.hand1PicBox5.TabStop = false;
+            // 
+            // hand1PicBox4
+            // 
+            this.hand1PicBox4.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand1PicBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand1PicBox4.Location = new System.Drawing.Point(439, 129);
+            this.hand1PicBox4.Name = "hand1PicBox4";
+            this.hand1PicBox4.Size = new System.Drawing.Size(64, 80);
+            this.hand1PicBox4.TabIndex = 3;
+            this.hand1PicBox4.TabStop = false;
+            // 
+            // hand1PicBox3
+            // 
+            this.hand1PicBox3.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand1PicBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand1PicBox3.Location = new System.Drawing.Point(360, 129);
+            this.hand1PicBox3.Name = "hand1PicBox3";
+            this.hand1PicBox3.Size = new System.Drawing.Size(64, 80);
+            this.hand1PicBox3.TabIndex = 2;
+            this.hand1PicBox3.TabStop = false;
+            // 
+            // hand1PicBox2
+            // 
+            this.hand1PicBox2.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand1PicBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand1PicBox2.Location = new System.Drawing.Point(280, 129);
+            this.hand1PicBox2.Name = "hand1PicBox2";
+            this.hand1PicBox2.Size = new System.Drawing.Size(64, 80);
+            this.hand1PicBox2.TabIndex = 1;
+            this.hand1PicBox2.TabStop = false;
+            // 
+            // hand1PicBox1
+            // 
+            this.hand1PicBox1.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand1PicBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand1PicBox1.Location = new System.Drawing.Point(194, 129);
+            this.hand1PicBox1.Name = "hand1PicBox1";
+            this.hand1PicBox1.Size = new System.Drawing.Size(64, 80);
+            this.hand1PicBox1.TabIndex = 0;
+            this.hand1PicBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = global::HW4CardGame.Properties.Resources.poker_table;
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.hand2PicBox5);
+            this.tabPage2.Controls.Add(this.hand2PicBox4);
+            this.tabPage2.Controls.Add(this.hand2PicBox3);
+            this.tabPage2.Controls.Add(this.hand2PicBox2);
+            this.tabPage2.Controls.Add(this.hand2PicBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 385);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Player 2 Hand";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // hand2PicBox5
+            // 
+            this.hand2PicBox5.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand2PicBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand2PicBox5.Location = new System.Drawing.Point(520, 129);
+            this.hand2PicBox5.Name = "hand2PicBox5";
+            this.hand2PicBox5.Size = new System.Drawing.Size(64, 80);
+            this.hand2PicBox5.TabIndex = 9;
+            this.hand2PicBox5.TabStop = false;
+            // 
+            // hand2PicBox4
+            // 
+            this.hand2PicBox4.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand2PicBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand2PicBox4.Location = new System.Drawing.Point(439, 129);
+            this.hand2PicBox4.Name = "hand2PicBox4";
+            this.hand2PicBox4.Size = new System.Drawing.Size(64, 80);
+            this.hand2PicBox4.TabIndex = 8;
+            this.hand2PicBox4.TabStop = false;
+            // 
+            // hand2PicBox3
+            // 
+            this.hand2PicBox3.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand2PicBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand2PicBox3.Location = new System.Drawing.Point(360, 129);
+            this.hand2PicBox3.Name = "hand2PicBox3";
+            this.hand2PicBox3.Size = new System.Drawing.Size(64, 80);
+            this.hand2PicBox3.TabIndex = 7;
+            this.hand2PicBox3.TabStop = false;
+            // 
+            // hand2PicBox2
+            // 
+            this.hand2PicBox2.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand2PicBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand2PicBox2.Location = new System.Drawing.Point(280, 129);
+            this.hand2PicBox2.Name = "hand2PicBox2";
+            this.hand2PicBox2.Size = new System.Drawing.Size(64, 80);
+            this.hand2PicBox2.TabIndex = 6;
+            this.hand2PicBox2.TabStop = false;
+            // 
+            // hand2PicBox1
+            // 
+            this.hand2PicBox1.BackgroundImage = global::HW4CardGame.Properties.Resources._53;
+            this.hand2PicBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hand2PicBox1.Location = new System.Drawing.Point(194, 129);
+            this.hand2PicBox1.Name = "hand2PicBox1";
+            this.hand2PicBox1.Size = new System.Drawing.Size(64, 80);
+            this.hand2PicBox1.TabIndex = 5;
+            this.hand2PicBox1.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackgroundImage = global::HW4CardGame.Properties.Resources.poker_table;
+            this.tabPage3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 385);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Result";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.generateHandsToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // generateHandsToolStripMenuItem
+            // 
+            this.generateHandsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generateHand1ToolStripMenuItem,
+            this.generateHand2ToolStripMenuItem});
+            this.generateHandsToolStripMenuItem.Name = "generateHandsToolStripMenuItem";
+            this.generateHandsToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.generateHandsToolStripMenuItem.Text = "Generate Hands";
+            // 
+            // generateHand1ToolStripMenuItem
+            // 
+            this.generateHand1ToolStripMenuItem.Name = "generateHand1ToolStripMenuItem";
+            this.generateHand1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.generateHand1ToolStripMenuItem.Text = "Generate Hand 1";
+            // 
+            // generateHand2ToolStripMenuItem
+            // 
+            this.generateHand2ToolStripMenuItem.Name = "generateHand2ToolStripMenuItem";
+            this.generateHand2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.generateHand2ToolStripMenuItem.Text = "Generate Hand 2";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.exitToolStripMenuItem.Text = "Evaluate Result";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "5 Card Poker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand1PicBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hand2PicBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox hand1PicBox5;
+        private System.Windows.Forms.PictureBox hand1PicBox4;
+        private System.Windows.Forms.PictureBox hand1PicBox3;
+        private System.Windows.Forms.PictureBox hand1PicBox2;
+        private System.Windows.Forms.PictureBox hand1PicBox1;
+        private System.Windows.Forms.PictureBox hand2PicBox5;
+        private System.Windows.Forms.PictureBox hand2PicBox4;
+        private System.Windows.Forms.PictureBox hand2PicBox3;
+        private System.Windows.Forms.PictureBox hand2PicBox2;
+        private System.Windows.Forms.PictureBox hand2PicBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateHandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateHand1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateHand2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
     }
 }
 
--- HW4CardGame/Form1.cs
+++ HW4CardGame/Form1.cs
@@ -12,9 +12,67 @@ namespace HW4CardGame
 {
     public partial class Form1 : Form
     {
+        // create one Random object to share among DeckOfCards objects
+        private static Random randomNumbers = new Random();
+
+        private const int NumberOfCards = 52; // number of cards in a deck
+        private Card[] deck = new Card[NumberOfCards]; // array of Card objects
+        private int currentCard = 0; // index of next Card to be dealt (0-51)
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // populates deck with objects of the Card class
+        private void CreateDeck() { 
+            string[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven",
+                            "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            for (var count = 0; count < deck.Length; ++count)
+            {
+                deck[count] = new Card(faces[count % 13], suits[count / 13]);
+            }
+        }
+
+        // shuffles deck of Cards
+        private void Shuffle()
+        {
+            // after shuffling, dealing restarts back at deck[0]
+            currentCard = 0;
+
+            // for each Card, pick another random Card and swap them
+            for (var first = 0; first < deck.Length; ++first)
+            {
+                // selects a random number between 0 and 51 
+                var second = randomNumbers.Next(NumberOfCards);
+
+                // swap current Card with the randomly selected Card
+                Card temp = deck[first];
+                deck[first] = deck[second];
+                deck[second] = temp;
+            }
+        }
+
+        // deals the next card in the deck
+        private Card DealCard()
+        {
+            // determine whether Cards remain to be dealt
+            if (currentCard < deck.Length)
+            {
+                // return current card in array
+                return deck[currentCard++];
+            }
+            else
+            {
+                // indicate all cards are dealt
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Shuffle();
         }
     }
 }
